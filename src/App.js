6e1ce4560aa15b83be1fba3c066ae7c23cd2e4cd
John Curry <fastpenguin91@gmail.com>
2020-12-01 09:48:22 -0800

    works with suspense

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 import React, { lazy } from "react";
 import { useAuth } from "./context/auth-context";
 import { FullPageSpinner } from "./components/lib";
-import UnauthenticatedApp from "./unauthenticated-app";
-import AuthenticatedApp from "./authenticated-app";
 
-// const AuthenticatedApp = lazy(
-//   () =>
-//     new Promise((resolve, reject) => {
-//       import("./authenticated-app")
-//         .then((result) =>
-//           resolve(result.default ? result : { default: result })
-//         )
-//         .catch(reject);
-//     })
-// );
-
-// const UnauthenticatedApp = lazy(
-//   () =>
-//     new Promise((resolve, reject) => {
-//       import("./unauthenticated-app")
-//         .then((result) =>
-//           resolve(result.default ? result : { default: result })
-//         )
-//         .catch(reject);
-//     })
-// );
-// const AuthenticatedApp = lazy(() => import("./authenticated-app"));
+const AuthenticatedApp = lazy(() => import("./authenticated-app"));
 // import "./App.css";
-// const UnauthenticatedApp = lazy(() => import("./unauthenticated-app"));
+const UnauthenticatedApp = lazy(() => import("./unauthenticated-app"));
 
 function App() {
-  // const user = null;
   const { user } = useAuth();
   console.log("user from app: ", user);
-  // return <UnauthenticatedApp />;
-  return <div>{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
+  return (
+    <React.Suspense fallback={<FullPageSpinner />}>
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+    </React.Suspense>
+  );
 }
 
 export { App };
