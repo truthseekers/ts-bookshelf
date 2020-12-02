dee1840022c3d6504196b3e7cc777c777c1f76ea
John Curry <fastpenguin91@gmail.com>
2020-12-02 09:30:22 -0800

    useupdatelistitem in status-buttons

--- src/components/status-buttons.js
+++ src/components/status-buttons.js
@@ -1,4 +1,9 @@
+import { useListItem, useUpdateListItem } from "../utils/list-items";
+
 function StatusButtons({ book }) {
+  const listItem = useListItem(book.id);
+
+  const [mutate] = useUpdateListItem({ throwOnError: true });
   return <div>status buttons.</div>;
 }
 
--- src/utils/list-items.js
+++ src/utils/list-items.js
@@ -1,5 +1,5 @@
 import { useClient } from "../context/auth-context";
-import { useQuery } from "react-query";
+import { useQuery, useMutation, queryCache } from "react-query";
 import { setQueryDataForBook } from "./books";
 
 function useListItem(bookId, options) {
@@ -26,4 +26,39 @@ function useListItems() {
   return [];
 }
 
-export { useListItem, useListItems };
+const defaultMutationOptions = {
+  onError: (err, variables, recover) =>
+    typeof recover === "function" ? recover() : null,
+  onSettled: () => queryCache.invalidateQueries("list-items"),
+};
+
+function onUpdateMutation(newItem) {
+  const previousItems = queryCache.getQueryData("list-items");
+
+  queryCache.setQueryData("list-items", (old) => {
+    return old.map((item) => {
+      return item.id === newItem.id ? { ...item, ...newItem } : item;
+    });
+  });
+
+  return () => queryCache.setQueryData("list-items", previousItems);
+}
+
+function useUpdateListItem(options) {
+  const client = useClient();
+
+  return useMutation(
+    (updates) =>
+      client(`list-items/${updates.id}`, {
+        method: "PUT",
+        data: updates,
+      }),
+    {
+      onMutate: onUpdateMutation,
+      ...defaultMutationOptions,
+      ...options,
+    }
+  );
+}
+
+export { useListItem, useListItems, useUpdateListItem };
